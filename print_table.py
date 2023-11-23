4d5cd6fb206fd178a09967d71254a50c49a29012
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2023-11-23 18:47:35 +0500

    Second version printing table 2x2

--- print_table.py
+++ print_table.py
@@ -1,18 +1,37 @@
+def print_cell_hor_line():
+    print(' -' * 4, end='')
+    print(' ', end='')
+
+def print_cell_corner():
+    print('+', end='')
+
+def print_cell_top():
+    print_cell_corner()
+    print_cell_hor_line()
+
+def print_cell_vert_line():
+    print('|', ' ' * 8, end='')
+
+def print_table_vert_one_line():
+    do_twice(print_cell_vert_line)
+    print('|')
+
+def print_table_vert_line():
+    do_twice(print_table_vert_one_line)
+    do_twice(print_table_vert_one_line)
+
+def do_twice(f):
+    f()
+    f()
+
 def print_table_top():
-    print('+', ' -' * 4, ' +', ' -'*4, ' +')
-
-def print_table_cell_line():
-    print('|', ' ' * 9, '|', ' ' * 9, '|')
-
-def print_table_cells():
-    print_table_cell_line()
-    print_table_cell_line()
-    print_table_cell_line()
-    print_table_cell_line()
+    do_twice(print_cell_top)
+    print_cell_corner()
+    print()
 
 
 print_table_top()
-print_table_cells()
+print_table_vert_line()
 print_table_top()
-print_table_cells()
+print_table_vert_line()
 print_table_top()
