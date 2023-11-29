a879a9c186890f03ceef74734a420cb35b1afe8c
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2023-11-29 15:58:52 +0500

    Excercise completed

--- check_fermat.py
+++ check_fermat.py
@@ -1,14 +1,29 @@
-def power(a, n):
+def power(k, n):
     if (n != 0):
-        print(a, n)
         n = n - 1
-        return a * power(a, n)
+        return k * power(k, n)
     else:
         return 1
 
 def check_fermat(a, b, c, n):
     if (n > 2):
-        pass
+        a_of_n = power(a, n)
+        b_of_n = power(b, n)
+        c_of_n = power(c, n)
 
+        if (a_of_n + b_of_n) == c_of_n:
+            print('Holly smokes, Fermat was wrong!')
+        else:
+            print("No, that doesn't work.")
+    else:
+        print("n must be greate than 2")
 
-print(power(2,2))
+def read_function_values():
+    a = int(input('Please enter the value for variable a='))
+    b = int(input('Please enter the value for variable b='))
+    c = int(input('Please enter the value for variable c='))
+    n = int(input('Please enter the value for variable n='))
+
+    check_fermat(a, b, c, n)
+
+read_function_values()
