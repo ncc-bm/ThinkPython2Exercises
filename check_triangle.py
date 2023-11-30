198eba01cd7c94bdcb59a5170bb94e48d48aea96
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2023-11-30 16:21:26 +0500

    Execrise 5.3 completed

--- check_triangle.py
+++ check_triangle.py
@@ -4,3 +4,11 @@ def is_triangle(a, b, c):
     else:
         return "No"
 
+def get_triangle_params():
+    a = input("Get the value for a side 'a'=")
+    b = input("Get the value for a side 'b'=")
+    c = input("Get the value for a side 'c'=")
+
+    print(is_triangle(int(a), int(b), int(c)))
+
+get_triangle_params()
