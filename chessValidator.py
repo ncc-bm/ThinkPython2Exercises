e302cd87da4a12ac597df52f2675693e11620420
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2023-12-26 22:05:48 +0500

    chessValidator.py added

---
+++ chessValidator.py
@@ -0,0 +1,14 @@
+chessBoar = {'1h': 'bking',
+             '6c': 'wqueen',
+             '2g': 'bbishop',
+             '5h': 'bqueen',
+             '3e': 'wking'}
+
+
+def isValidChessBoard(dict):
+    for i, k in dict:
+        print(k)
+    return True
+
+
+isValidChessBoard(chessBoar)
