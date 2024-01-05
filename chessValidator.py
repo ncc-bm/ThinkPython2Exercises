a14d645305cb46d69e48c91461ab024ff03ecbe5
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-01-05 18:39:59 +0500

    a dictionary wht a valid number chess pieces added

--- chessValidator.py
+++ chessValidator.py
@@ -1,15 +1,36 @@
+chessValidPieces = {
+    'wpawn': 8,
+    'bpawn': 8,
+    'wbishop': 2,
+    'bbishop': 2,
+    'wknight': 2,
+    'bknight': 2,
+    'wrook': 2,
+    'brook': 2,
+    'wqueen': 1,
+    'bqueen': 1,
+    'wking': 1,
+    'bking': 1,
+
+}
 chessBoard = {'1h': 'bking',
              '6c': 'wqueen',
              '2g': 'bbishop',
              '5h': 'bqueen',
              '3e': 'wking'}
 
+def isValidNumberPieces(dict):
+    return len(dict) <= 32
+
+#def countFigures(dict):
+
 
 def isValidChessBoard(dict):
-    #for k, v in dict.items():
-    #    print("key is: " + k + ' value is: ' + v)
+    for k, v in dict.values():
+        print(k)
     #print(list(dict.keys()))
-    print('wking' in dict.values())
+    #print('wking' in dict.values())
+    print(isValidNumberPieces(dict))
     
 
 isValidChessBoard(chessBoard)
