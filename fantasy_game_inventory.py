8e667b18c0ce5318c5ec72276c469dc554fe1673
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-01-23 17:34:48 +0500

    addToInventory() function added

--- fantasy_game_inventory.py
+++ fantasy_game_inventory.py
@@ -1,4 +1,8 @@
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'ruby']
+
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+dragonLoot2 = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby', 'tilla tanga', 'kumush tanga']
 
 def displayInventory(inventory):
     print('Inventory:')
@@ -8,5 +12,17 @@ def displayInventory(inventory):
         item_total += v
     print('Total number of items: ' + str(item_total))
 
+def addToInventory(inventory, addedItems):
+    for i in addedItems:
+        inventory[i] = inventory.get(i, 0) + 1
+    return inventory
 
+
+
+displayInventory(stuff)
+stuff = addToInventory(stuff, dragonLoot)
+print()
+displayInventory(stuff)
+print()
+stuff = addToInventory(stuff, dragonLoot2)
 displayInventory(stuff)
