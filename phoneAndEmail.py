44e6dcdf0dd15ff038e3471d36cc42a160720c59
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-02-26 17:38:18 +0500

    Copy restul to the clipboard

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -38,4 +38,10 @@ for groups in emailRegex.findall(text):
     
 
 
-# TODO: Copy results to the clipboard.
+# Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
