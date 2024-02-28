febcbe16e6744d8a2a24f7f5d214de096953049d
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-02-28 12:52:12 +0500

    1. Regex to select all interfaces with configuration section added 2. Regex to select all interfaces only names added 3. Regex to select interfaces with specific names added

--- ciscoConfigParser.py
+++ ciscoConfigParser.py
@@ -3,25 +3,38 @@
 
 import pyperclip, re
 
-# All sections wich starts with the string "interface" and ends with '!' regex
+# Select all interfaces with configuration
 interfaceConfigRegex = re. compile(r'''(
-    ^interface                       # matched text should start with a string "interface" with preceding spaces
+    ^interface                          # matched text should start with a string "interface" with preceding spaces
     .*?                                 # there should be a configuration of the interface
     \!                                  # interface configuration section should end '!' sign at the end
 )''', re.VERBOSE|re.DOTALL|re.MULTILINE)
+
+# Select all interfaces (only names)
+allInterfaceRegex = re. compile(r'''(
+    ^interface                         # matched text should start with a string "interface" with preceding spaces
+    .*                                 # there should type and name of the interface
+)''', re.VERBOSE|re.MULTILINE)
+
+# Select necessary interfaces (only names)
+interfaceRegex = re. compile(r'''(
+    ^interface.*                                            # matched text should start with a string "interface" with preceding spaces
+    (Bundle-Ether.*|GigabitEthernet.*|TenGig.*|Hundred.*)   # select specific interfaces
+    .*
+ )''', re.VERBOSE|re.MULTILINE)
 
 
 # Find matches in clipboard text.
 text = str(pyperclip.paste())
 matches = []
 
-for groups in interfaceConfigRegex.findall(text):
-    matches.append(groups)
+for groups in interfaceRegex.findall(text):
+    matches.append(groups[0])
 
 # Copy results to the clipboard.
 if len(matches) > 0:
-   pyperclip.copy('\n'.join(matches))
-   print('Copied to clipboard:')
-   print('\n'.join(matches))
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
 else:
     print('No interfaces found.')
