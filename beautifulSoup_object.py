8cdcd92f6bd2baac2f077b82a61a462eb46a7c29
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-06 23:23:47 +0500

    using faster 'lxml' parser

--- beautifulSoup_object.py
+++ beautifulSoup_object.py
@@ -12,5 +12,5 @@ print(type(noStarchSoup))
  """
 
 exampleFile = open('example.html')
-exampleSoup = bs4.BeautifulSoup(exampleFile, 'html.parser')
+exampleSoup = bs4.BeautifulSoup(exampleFile, 'lxml')
 print(type(exampleSoup))
