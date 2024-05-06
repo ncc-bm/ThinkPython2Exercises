f719ee2521d1aa1fc6c0bf03355c0c0318100a32
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-06 23:12:52 +0500

    load a web page from Internet for bs4 parsing

---
+++ beautifulSoup_object.py
@@ -0,0 +1,12 @@
+import requests, bs4
+
+res = requests.get('https://nostarch.com')
+
+try:
+    res.raise_for_status()
+except Exception as excp:
+    print('There was a problem %s' % (excp))
+    
+noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+print(type(noStarchSoup))
+
