6adaf4354349edc97cfe09ac2ee6031f3c150ce0
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-06 23:21:30 +0500

    Load local file to parse with bs4

--- beautifulSoup_object.py
+++ beautifulSoup_object.py
@@ -1,6 +1,6 @@
 import requests, bs4
 
-res = requests.get('https://nostarch.com')
+""" res = requests.get('https://nostarch.com')
 
 try:
     res.raise_for_status()
@@ -9,4 +9,8 @@ except Exception as excp:
     
 noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
 print(type(noStarchSoup))
+ """
 
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile, 'html.parser')
+print(type(exampleSoup))
