cfc81122e2a8010acefab0c6e151515bb2c4413a
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-06 11:24:04 +0500

    Coin Toss with problem (original)

---
+++ coinToss_Debug.py
@@ -0,0 +1,18 @@
+import random
+
+guess = ''
+
+while guess not in ('heads', 'tails'):
+    print('Guess the coint toss! Enter heads or tails:')
+    guess = input()
+
+toss = random.choice(['heads', 'tails']) # 0 is tails, 1 is heads
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
