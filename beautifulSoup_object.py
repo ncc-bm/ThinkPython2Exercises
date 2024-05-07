483771e4f344de667474f7d293cafaca17c85543
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-07 22:52:48 +0500

    Selecting(Getting) all <p> elements

--- beautifulSoup_object.py
+++ beautifulSoup_object.py
@@ -13,7 +13,7 @@ print(type(noStarchSoup))
 
 exampleFile = open('example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile, 'lxml')
-print(type(exampleSoup))
+""" print(type(exampleSoup))
 
 elems = exampleSoup.select('#author')
 print(type(elems))      # elems is a list of Tag objects.
@@ -21,4 +21,15 @@ print(len(elems))       # the length is 1
 print(type(elems[0]))   # <class 'bs4.element.Tag'>
 print(str(elems[0]))    # The Tag object as a string. <span id="author"> Al Sweigart</span>
 print(elems[0].getText()) # Al Sweigart
-print(elems[0].attrs)   # {'id': 'author'}
+print(elems[0].attrs)   # {'id': 'author'} """
+
+
+pElems = exampleSoup.select('p')
+print(str(pElems[0]))
+print(pElems[0].getText())
+print()
+print(str(pElems[1]))
+print(pElems[1].getText())
+print()
+print(str(pElems[2]))
+print(pElems[2].getText())
