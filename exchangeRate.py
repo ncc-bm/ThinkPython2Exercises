426761d9a2c184cdf38841aa954eef0ea6a010a8
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-08 16:11:17 +0500

    Aloqabank exchange rante added

--- exchangeRate.py
+++ exchangeRate.py
@@ -1,6 +1,8 @@
 import requests, webbrowser, bs4, os
 
 nbu_exchange_rate_url = 'https://nbu.uz/uz/exchange-rates/'
+aloqabank_exchange_rate_url = 'https://aloqabank.uz/ru/services/exchange-rates/'
+
 
 def get_nbu_exchange_rate(url):
     web_page = requests.get(url)
@@ -13,21 +15,29 @@ def get_nbu_exchange_rate(url):
     
     return exchange_table[1]
 
-""" web_page = requests.get(nbu_exchange_rate_url)
-try:
-    web_page.raise_for_status()
-except Exception as excpt:
-    print('There is a problem with %s' % (excpt))
+def get_aloqabank_exchange_rate(url):
+    web_page = requests.get(url)
+    try:
+        web_page.raise_for_status()
+    except Exception as excpt:
+        print('There is a problem with %s' % (excpt))
 
-soup = bs4.BeautifulSoup(web_page.text, 'lxml')
-exchange_table = soup.select('.kursdata')
- """
+    soup = bs4.BeautifulSoup(web_page.text, 'lxml')
+    exchange_table = soup.select('div[data-tabs-target="tab2"] table')
+
+    return exchange_table[0]
+
+
 
 exchange_rate_file = open('exchange.html', 'w')
 exchange_rate_file.write('<html><body>')
 
 exchange_rate_file.write('<h1>NBU Exchange Rate</h1>')
 exchange_rate_file.write(str(get_nbu_exchange_rate(nbu_exchange_rate_url)))
+
+exchange_rate_file.write('<br>')
+exchange_rate_file.write('<h1>Aloqabank Exchange Rate</h1>')
+exchange_rate_file.write(str(get_aloqabank_exchange_rate(aloqabank_exchange_rate_url)))
 
 exchange_rate_file.write('</body></html>')
 
