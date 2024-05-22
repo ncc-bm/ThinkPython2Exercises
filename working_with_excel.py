d526b17c7bd847257ecc0369399d79045d459ab9
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-22 14:34:00 +0500

    Converting between colum letters and numbers

--- working_with_excel.py
+++ working_with_excel.py
@@ -1,8 +1,9 @@
 import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
 
 wb = openpyxl.load_workbook('example.xlsx')
 
-
+""" 
 print(type(wb))
 
 print(wb.sheetnames)
@@ -15,5 +16,38 @@ print(sheet.title)
 anotherSheet = wb.active
 print(anotherSheet)
 print(anotherSheet.title)
+ """
 
+sheet = wb['Sheet1']
+print(sheet['A1'])
+print(sheet['A1'].value)
 
+c = sheet['B1']
+print(c.value)
+print('Row %s, Column %s is %s' % (c.row, c.column, c.value))
+print('Row %s, Column %s is %s' % (c.row, get_column_letter(c.column), c.value))
+print('Cell %s is %s' % (c.coordinate, c.value))
+print(sheet['C1'].value)
+
+print()
+print(sheet.cell(row=1, column=2))
+print(sheet.cell(row=1, column=2).value)
+
+for i in range(1, 8, 2):
+    print(i, sheet.cell(row=i, column=2).value)
+
+print()
+print(sheet.max_row)
+print(sheet.max_column)
+
+print()
+print(get_column_letter(1))
+print(get_column_letter(2))
+print(get_column_letter(27))
+print(get_column_letter(900))
+print(get_column_letter(c.column))
+
+print(get_column_letter(sheet.max_column))
+
+print(column_index_from_string('A'))
+print(column_index_from_string('AA'))
