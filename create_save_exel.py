bce1ca961cb5dc3629d992b1223aa26d62bf7a67
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-23 15:25:03 +0500

    Creating and removing sheets

--- create_save_exel.py
+++ create_save_exel.py
@@ -2,12 +2,29 @@ import openpyxl
 
 wb = openpyxl.Workbook() # create a blank workbook
 print(wb.sheetnames)
+print()
 
 sheet = wb.active
 print(sheet.title)
+print()
 
 sheet.title = 'Spam Bacon Eggs Sheet' # Change title
 print(wb.sheetnames)
+print()
 
+""" 
 sheet.title = 'Spam Spam Spam'
 wb.save('example_copy.xlsx')
+ """
+
+wb.create_sheet()
+print(wb.sheetnames)
+print()
+
+wb.create_sheet(index=0, title='First Sheet')
+print(wb.sheetnames)
+print()
+
+wb.create_sheet(index=2, title='Middle Sheet')
+print(wb.sheetnames)
+print()
