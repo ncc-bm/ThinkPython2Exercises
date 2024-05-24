0bde988fdd60086a76b412ad3157c2f4c0da2a08
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-05-24 18:30:12 +0500

    storing font object in a variables and apply to cells

--- styling_xlsx.py
+++ styling_xlsx.py
@@ -6,4 +6,14 @@ sheet = wb['Sheet']
 italic24Font = Font(size=24, italic=True) # Create a font
 sheet['A1'].font = italic24Font # Apply the font to A1.
 sheet['A1'] = 'Hello world!'
-wb.save('syles.xlsx')
+wb.save('styles.xlsx')
+
+fontObj1 = Font(name='Times New Roman', bold=True)
+sheet['B1'].font = fontObj1
+sheet['B1'] = 'Bold Time New Roman'
+
+fontObj2 = Font(size=24, italic=True)
+sheet['B3'].font = fontObj2
+sheet['B3'] = '24 pt Italic'
+
+wb.save('styles.xlsx')
