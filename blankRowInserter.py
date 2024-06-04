ec353adcba5a0ae358c7027fbd30350b21479ab6
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-06-04 17:39:49 +0500

    task completed

--- blankRowInserter.py
+++ blankRowInserter.py
@@ -1,2 +1,10 @@
 import sys, openpyxl
 
+if (len(sys.argv) < 4) or (len(sys.argv) > 4):
+    print('Usage python blankRowInserter.py starti_row row_num_to_insert file_name')
+
+wb = openpyxl.load_workbook(sys.argv[3])
+sheet = wb.active
+sheet.insert_rows(int(sys.argv[1]), int(sys.argv[2]))
+
+wb.save('produceSales_newRows.xlsx')
