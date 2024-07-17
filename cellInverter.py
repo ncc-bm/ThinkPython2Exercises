0811e8b52ae73e9d22604c221ef59b8fa74cf4a0
Bakhtiyor Mazgarov <mazgarov@uznet.net>
2024-07-17 10:04:28 +0500

    task partially finished

--- cellInverter.py
+++ cellInverter.py
@@ -9,8 +9,8 @@ elif len(sys.argv) < 2:
 else:
     fileName = sys.argv[1]
 
-#print(fileName)
-if not(os.path.isfile(fileName)):
+# print(fileName)
+if not (os.path.isfile(fileName)):
     print('%s file does not exists. Please enter valid file name!' % fileName)
 
 wb_org = openpyxl.load_workbook(fileName)
@@ -24,7 +24,7 @@ wb_inverted = openpyxl.Workbook()
 sheet_inverted = wb_inverted.active
 
 for row in range(1, sheet_org.max_row+1):
-    for column in range (1, sheet_org.max_column+1):
+    for column in range(1, sheet_org.max_column+1):
         sheet_inverted.cell(row=column, column=row).value = sheet_org.cell(row=row, column=column).value
 
 wb_inverted.save('invertedCells.xlsx')
